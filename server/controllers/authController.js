31104d9c741b4382d82a768ec3d39cd30773cfe7
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-04-19 13:40:36 -0400

    adding signup endpoint and validate signup helper method

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,91 @@
-exports.validateSignup = () => {};
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
-exports.signup = () => {};
+/**
+ * @function validateSignup
+ * @param {object} req
+ * @param {object} res
+ * @param {function} next
+ * @summary Middleware function that will help validate data from form
+ * inputs
+ * @returns
+ */
+const validateSignup = (req, res, next) => {
+  /**
+   * sanitizedBoy is an Express-Validator method which
+   * used to prevent insertion of malicious code in
+   */
+  req.sanitizeBody("name");
+  req.sanitizeBody("email");
+  req.sanitizeBody("password");
 
-exports.signin = () => {};
+  /**
+   *  In the Name field make sure it is non-null and
+   * is 4 to 10 characters long using Express-Validator's checkBody
+   */
+  req.checkBody("name", "Enter a name").notEmpty();
+  req
+    .checkBody("name", "Name must be between 4 and 10 characters")
+    .isLength({ min: 4, max: 10 });
 
-exports.signout = () => {};
+  /**
+   *  In the Email field make sure it is non-null, valid, and
+   *  normalized, using Express-Validator's checkBody
+   */
 
-exports.checkAuth = () => {};
+  req.checkBody("email", "Enter a valid email").isEmail().normalizeEmail();
+
+  /**
+   *  In the Passowrd field make sure it is non-null and
+   * is 4 to 10 characters long using Express-Validator's checkBody
+   */
+  req.checkBody("password", "Enter a password").notEmpty();
+  req
+    .checkBody("password", "Password must be between 4 and 10 characters")
+    .isLength({ min: 4, max: 10 });
+
+  const errors = req.validationErrors();
+  if (errors) {
+    const firstError = errors.map((error) => error.msg)[0];
+    return res.status(400).send(firstError);
+  }
+  next();
+};
+
+/**
+ * @function signup
+ * @param {object} req
+ * @param {object} res
+ * @returns {void}
+ */
+const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await new User({ name, email, password });
+  await User.register(user, password, (err, user) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.json(user);
+  });
+};
+
+/**
+ * @function
+ */
+const signin = () => {};
+/**
+ * @function
+ */
+const signout = () => {};
+/**
+ * @function
+ */
+const checkAuth = () => {};
+
+module.exports = {
+  validateSignup,
+  signup,
+  signin,
+  signout,
+  checkAuth,
+};
